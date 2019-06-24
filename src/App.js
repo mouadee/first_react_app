1abf4080d367e565c95a458e4480bc4ba10ecc5f
Vue Js commit <elazzaouimouade@gmail.com>
2019-06-24 08:30:03 +0100

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-
-
-
-
-
-
-
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is our first react app
-        </p>
-        <a className="App-link" href="#">
-          Mouad first react App
-        </a>
-      </header>
-    </div>
+    React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'))
   );
 }
 
